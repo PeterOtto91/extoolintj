9d7aabc3809d3b65a1978eb3464086d038587549
PeterOtto91 <peter.otto@telenet.be>
2020-09-28 16:26:48 +0200

    exb6

---
+++ ExB6/src/be/abis/exB6/model/Address.java
@@ -0,0 +1,40 @@
+package be.abis.exB6.model;
+
+public class Address {
+
+    private String street;
+    private String nr;
+    private String zipCode;
+
+    public Address(String street, String nr, String zipCode) {
+        this.street = street;
+        this.nr = nr;
+        this.zipCode = zipCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNr() {
+        return nr;
+    }
+
+    public void setNr(String nr) {
+        this.nr = nr;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+
+}
---
+++ ExB6/src/be/abis/exB6/test/ExB6.java
@@ -0,0 +1,14 @@
+package be.abis.exB6.test;
+
+import be.abis.exB6.model.Address;
+
+public class ExB6{
+
+public static void main(String[]args){
+
+    Address a = new Address("AaStraat", "5", "2800");
+    System.out.println(a.getStreet());
+
+        }
+
+}
